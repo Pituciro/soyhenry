d7543831c73109d456a50bf796a62b28c8d64da3
pituciro <pitu.ciro@hotmail.comgit config -- user.name pitucirogit commit -m first>
2022-05-25 16:32:29 -0300

    hw3

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,7 +19,11 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
-}
+  var contador = 1;
+  return function (){
+          return contador++
+    }
+ }
 
 function cacheFunction(cb) {
   /*
@@ -41,6 +45,16 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+ var memoria = {};
+  return function(arg){
+    if (memoria.hasOwnProperty(arg)){
+      return memoria[arg];
+    }
+    else{
+      memoria[arg] =cb(arg)
+    }
+    return memoria[arg];
+  }
 }
 
 // Bind
@@ -67,22 +81,25 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
   
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, 
+  tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, 
+    respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - 
+    ya que los otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(this,"*", "*");
+let textoGuiones= crearCadena.bind(this, "-", "-");
+let textoUnderscore = crearCadena.bind(this,"_","_");
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
